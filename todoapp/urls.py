d70f2f1584f3550411f8e5422049e16ba3c42d76
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-09-28 08:17:41 +0100

    Updated urls and views

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('task-list/', views.task_list, name='task-list'),
+    path('task-detail/<int:pk>/', views.task_detail, name='task-detail'),
+    path('task-create/', views.task_create, name='task-create'),
+    path('task-update/<int:pk>/', views.task_update, name='task-update'),
+    path('task-delete/<int:pk>/', views.task_delete, name='task-delete'),
+    
     path('add', views.add, name='add'),
     path('delete/<int:id>', views.delete, name='delete'),
     path('update/<int:id>', views.update, name='update'),
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 
 # Create your views here.
@@ -17,7 +19,26 @@ def index(request):
             'Delete': '/task-delete/<str:pk>/',
         }
     })
-    
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+def task_detail(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(task)
+    return Response(serializer.data)
+
+def task_create(request):
+    return Response({'message': 'Hello World'})
+
+def task_update(request, pk):
+    return Response({'message': 'Hello World'})
+
+def task_delete(request, pk):
+    return Response({'message': 'Hello World'})
 
 @api_view(['POST'])
 def add(request):
