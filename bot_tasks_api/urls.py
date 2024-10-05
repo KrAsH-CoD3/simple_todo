56030b8ce911755e1611bec405fa486de6462b3e
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-10-05 10:59:00 +0100

    Refactor names

--- bot_tasks_api/urls.py
+++ bot_tasks_api/urls.py
@@ -3,9 +3,9 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns: list[URLPattern] = [ 
-    path('save-task/', views.save_bot_task, name='save-bot-task'),
+    path('save-task/', views.save_task, name='save-task'),
     path('get-task/<str:title>/', views.get_task, name='get-task'),
-    path('tasks/', views.get_all_bot_tasks, name='get-all-bot-tasks'),
-    path('delete-task/<str:title>/', views.delete_bot_task, name='delete-bot-task'),
-    path('update-task/<str:title>/', views.update_bot_task, name='update-bot-task'),
+    path('tasks/', views.get_all_tasks, name='get-all-bot-tasks'),
+    path('delete-task/<str:title>/', views.delete_task, name='delete-task'),
+    path('update-task/<str:title>/', views.update_task, name='update-task'),
 ]
--- bot_tasks_api/views.py
+++ bot_tasks_api/views.py
@@ -3,19 +3,19 @@ from django.db.models.manager import BaseManager
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import TaskSerializer
+from .serializers import BotTaskSerializer
 from .models import BotTask
 
 @api_view(['GET'])
 def get_task(request, title) -> Response:
     tasks: BotTask = BotTask.objects.get(title=title) 
-    serializer = TaskSerializer(tasks)
+    serializer = BotTaskSerializer(tasks)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def get_all_bot_tasks(request) -> Response:
+def get_all_tasks(request) -> Response:
     tasks: BaseManager[BotTask] = BotTask.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
+    serializer = BotTaskSerializer(tasks, many=True)
     
     # for item in serializer.data:
     #     item['owner'] = 'custom_value'
@@ -26,8 +26,8 @@ def get_all_bot_tasks(request) -> Response:
     })
 
 @api_view(['POST'])
-def save_bot_task(request) -> Response:
-    tasks = TaskSerializer(data=request.data)
+def save_task(request) -> Response:
+    tasks = BotTaskSerializer(data=request.data)
     if not tasks.is_valid():
         return Response(tasks.errors, status=status.HTTP_400_BAD_REQUEST)
     tasks.save()
@@ -35,10 +35,11 @@ def save_bot_task(request) -> Response:
     return Response({
         'status': 'success',
         'message': 'Tasks saved successfully ✅',
+        "data": tasks.data,
     }, status=status.HTTP_201_CREATED)
 
 @api_view(['DELETE'])
-def delete_bot_task(request, title) -> Response:
+def delete_task(request, title) -> Response:
     tasks: BotTask = BotTask.objects.get(title=title)
     tasks.delete()
 
@@ -48,9 +49,9 @@ def delete_bot_task(request, title) -> Response:
     }, status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['PATCH'])
-def update_bot_task(request, title) -> Response:
+def update_task(request, title) -> Response:
     tasks: BotTask = BotTask.objects.get(title=title)
-    serializer = TaskSerializer(instance=tasks, data=request.data)
+    serializer = BotTaskSerializer(instance=tasks, data=request.data)
     if not serializer.is_valid():
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
     serializer.save()
