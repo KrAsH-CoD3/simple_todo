a8e073e63839e1421eb1f919bb8a06a99be2973e
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-10-05 10:57:58 +0100

    Made `created` field unique in models

--- bot_tasks_api/models.py
+++ bot_tasks_api/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-from django.utils import timezone
+# from rest_framework.serializers import ValidationError
 
 class BotTask(models.Model):
     owner = models.CharField(max_length=50)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     data = models.JSONField()
     created = models.DateTimeField(auto_now_add=True)
     execution_count = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return str(self.title)
+    
+    # def validate_title(self, value):
+    #     """
+    #     Check that the title is not already in the database.
+    #     """
+    #     if BotTask.objects.filter(title=value).exists():
+    #         raise ValidationError('Title already exists')
+    #     return value
