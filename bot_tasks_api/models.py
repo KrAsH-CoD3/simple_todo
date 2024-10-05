f5af73f1af805a1ee0eca287707478d2fce5fb33
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-10-05 09:39:49 +0100

    Formatted serializers `created` field to date and time

--- bot_tasks_api/models.py
+++ bot_tasks_api/models.py
@@ -5,7 +5,7 @@ class BotTask(models.Model):
     owner = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     data = models.JSONField()
-    created = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
     execution_count = models.IntegerField(default=0)
 
     def __str__(self) -> str:
--- bot_tasks_api/serializers.py
+++ bot_tasks_api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import BotTask
 
 class TaskSerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
     class Meta:
         model = BotTask
         fields: list[str] = ['id', 'owner', 'title', 'data', 'created', 'execution_count']
