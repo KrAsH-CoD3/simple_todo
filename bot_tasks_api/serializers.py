31c4672153e73dbbc995f7ef1d9d31deadb46ebb
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-10-05 10:54:09 +0100

    Added read only to `created` field

--- bot_tasks_api/serializers.py
+++ bot_tasks_api/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import BotTask
 
-class TaskSerializer(serializers.ModelSerializer):
-    created = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
+class BotTaskSerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)  # Ensure it's marked as read-only
+
     class Meta:
         model = BotTask
         fields: list[str] = ['id', 'owner', 'title', 'data', 'created', 'execution_count']
-        read_only_fields: list[str] = ['id']
+        # read_only_fields: list[str] = ['id', 'created', 'execution_count']
