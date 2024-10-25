63ef6a4e4182ac6d31ec462c5fe4a329c426fa8f
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-10-25 07:55:53 +0100

    Formatted/updated settings dev and prod

--- todo/settings/dev.py
+++ todo/settings/dev.py
@@ -1,26 +1,31 @@
+import dj_database_url
 from .common import *
 
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = []
+
+USE_SERVER_DB = True
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    # 'default': dj_database_url.parse(environ.get('DATABASE_URL')),
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': environ.get('PG_DATABASE_NAME'),
-        'USER': environ.get('PG_DATABASE_USER'),
-        'PASSWORD': environ.get('PG_DATABASE_PASSWORD'),
-        # 'HOST': environ.get('PG_DATABASE_HOST'),
-        # 'PORT': environ.get('PG_DATABASE_PORT'),
-    }
+    'default': (
+        dj_database_url.parse(environ.get('RENDER_DATABASE_URL')) 
+        if USE_SERVER_DB else {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': environ.get('PG_DATABASE_NAME'),
+            'USER': environ.get('PG_DATABASE_USER'),
+            'PASSWORD': environ.get('PG_DATABASE_PASSWORD'),
+            # 'HOST': 'localhost', # environ.get('PG_DATABASE_HOST'),
+            # 'PORT': '5432' # environ.get('PG_DATABASE_PORT'),
+        }
+    )
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
--- todo/settings/prod.py
+++ todo/settings/prod.py
@@ -1,18 +1,14 @@
 from .common import *
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     'simple-todo-5824.onrender.com',
-    # '127.0.0.1:3000',
 ]
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-import dj_database_url
 
+import dj_database_url
 DATABASES = {
     'default': dj_database_url.parse(environ.get('RENDER_DATABASE_URL'))
 }
