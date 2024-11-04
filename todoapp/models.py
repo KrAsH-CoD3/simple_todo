0b0ce1b2516d577f76c22ccaef56561f0cbfc3eb
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-11-04 15:25:24 +0100

    Using get_user_model to get appropriate user model

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User # NOT THE BEST PRACTICE
+from django.contrib.auth import get_user_model # BEST PRACTICE: To get the current active user model
 # from django.conf import settings
 
 
 # Create your models here.
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
     
