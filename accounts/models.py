72c87622cc234dc1fb01fc9f1e4604edb05fd129
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-11-04 15:27:06 +0100

    Added verbose names for the model

--- accounts/models.py
+++ accounts/models.py
@@ -53,10 +53,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username'] # For creating superuser using `createsuperuser` command
 
-    verbose_name = 'User'
-    verbose_name_plural = 'Users'
-
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
