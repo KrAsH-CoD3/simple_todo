6f38baad6c38aa0ffbb89e11e97e488c37303653
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-11-04 13:42:22 +0100

    Added comments for verbose meaning in bot_task_api model

--- bot_tasks_api/models.py
+++ bot_tasks_api/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 class BotTask(models.Model):
-    owner = models.CharField(max_length=50) # APIKEY
-    title = models.CharField(max_length=50, unique=True)
+    # verbose_name as a parameter in the field definition is the name that will appear in the form(django admin as an example)
+    owner = models.CharField(max_length=50, verbose_name="Owner(API Key)") 
+    title = models.CharField(max_length=50, unique=True, verbose_name="Task Title")
     data = models.JSONField()
     created = models.DateTimeField(auto_now_add=True)
     execution_count = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return str(self.title)
+    
+    class Meta: # This is for the admin dashboard
+        verbose_name: str = "Bot Task 😁" # How it be would be displayed when creating new task from the admin dashboard
+        verbose_name_plural: str = "Bot Tasks 😈" # How it be would be displayed in the admin dashboard at the side(if you wanna view all bot tasks)
